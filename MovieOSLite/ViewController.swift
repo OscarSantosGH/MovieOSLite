1f72d241f9921f2fc604c2631165b6aa6c5f4840
Oscar Santos <oscarsantos.go@gmail.com>
2020-01-17 18:41:16 -0500

    Initial Commit

---
+++ MovieOSLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieOSLite
+//
+//  Created by Oscar Santos on 1/17/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
