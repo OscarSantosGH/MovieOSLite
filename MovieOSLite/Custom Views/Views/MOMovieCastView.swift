0446ac582532a35dc669b0ee02a4409c15ac7a49
Oscar Santos <oscarsantos.go@gmail.com>
2020-03-22 19:18:05 -0400

    added: MOMovieCastView

---
+++ MovieOSLite/Custom Views/Views/MOMovieCastView.swift
@@ -0,0 +1,94 @@
+//
+//  MOMovieCastView.swift
+//  MovieOSLite
+//
+//  Created by Oscar Santos on 3/22/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+
+class MOMovieCastView: UIView {
+
+    let castLabel = MOTitleLabel(ofSize: 15, textAlignment: .left)
+    var collectionView: UICollectionView!
+    
+    var cast:[Actor] = []
+    var movieID: Int!
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        castLabel.text = "The Cast"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(withMovie id:Int) {
+        self.init(frame: .zero)
+        movieID = id
+        getCast()
+        configureCollectionView()
+        layoutUI()
+    }
+    
+    private func layoutUI(){
+        addSubviews(castLabel, collectionView)
+        
+        let padding:CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            castLabel.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            castLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            castLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            castLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            collectionView.topAnchor.constraint(equalTo: castLabel.bottomAnchor, constant: padding),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func configureCollectionView(){
+        collectionView = UICollectionView(frame: bounds, collectionViewLayout: UIHelper.createThreeColumnsLayout(in: self))
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(CastCell.self, forCellWithReuseIdentifier: CastCell.reuseID)
+    }
+    
+    private func getCast(){
+        NetworkManager.shared.getCast(from: movieID) { [weak self] result in
+            guard let self = self else {return}
+            switch result{
+            case .failure(let error):
+                print(error.rawValue)
+                break
+            case .success(let cast):
+                self.cast = cast
+                self.collectionView.reloadData()
+                break
+            }
+        }
+    }
+}
+
+extension MOMovieCastView: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        cast.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CastCell.reuseID, for: indexPath) as! CastCell
+        cell.set(actor: cast[indexPath.item])
+        return cell
+    }
+    
+}
+
+extension MOMovieCastView: UICollectionViewDelegate{
+    
+}
