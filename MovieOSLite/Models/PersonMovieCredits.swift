5fb04e80c9128158191ee2463bf4c8db2d76204b
Oscar Santos <oscarsantos.go@gmail.com>
2020-08-16 10:01:28 -0400

    added: PersonMovieCredits to get all the movies that a person is known for

---
+++ MovieOSLite/Models/PersonMovieCredits.swift
@@ -0,0 +1,16 @@
+//
+//  PersonMovieCredits.swift
+//  MovieOSLite
+//
+//  Created by Oscar Santos on 8/16/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import Foundation
+
+struct PersonMovieCredits: Codable, Hashable {
+    let id:Int
+    let title:String
+    let character:String
+    let posterPath:String
+}
