8a9861fe72986b0afa9e4f763f9e59cf451d5c10
Oscar Santos <oscarsantos.go@gmail.com>
2024-03-19 09:02:38 -0700

    Remove unused code in `MOImageLoaderView`

--- MovieOSVision/Custom Views/MOImageLoaderView.swift
+++ MovieOSVision/Custom Views/MOImageLoaderView.swift
@@ -9,8 +9,6 @@
 import SwiftUI
 
 struct MOImageLoaderView: View {
-    private let basePath = "https://image.tmdb.org/t/p/"
-    private let baseYoutubeThumbUrl = "https://img.youtube.com/vi/"
     var imagePath: String?
     var imageType: ImageType = .poster
     @State private var image: UIImage?
@@ -56,24 +54,6 @@ struct MOImageLoaderView: View {
     
     enum ImageType {
         case backdrop, poster, cast, trailer
-    }
-    
-    private var imageSize: String {
-        switch imageType {
-        case .backdrop:
-            "w300"
-        case .poster, .cast:
-            "w342"
-        default: ""
-        }
-    }
-    
-    private var url: URL? {
-        switch imageType {
-        case .trailer:
-            URL(string: baseYoutubeThumbUrl + (imagePath ?? "") + "/0.jpg")
-        default: URL(string: basePath + imageSize + (imagePath ?? ""))
-        }
     }
     
 }
