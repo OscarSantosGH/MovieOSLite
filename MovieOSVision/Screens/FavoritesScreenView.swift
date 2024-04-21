a313bdcd2bd7095cbff63e18aa37f37c85b659b9
Oscar Santos <oscarsantos.go@gmail.com>
2024-04-21 16:00:04 -0700

    Create `FavoritesScreenView` to see favorites movies

---
+++ MovieOSVision/Screens/FavoritesScreenView.swift
@@ -0,0 +1,52 @@
+//
+//  FavoritesScreenView.swift
+//  MovieOSVision
+//
+//  Created by Oscar Santos on 4/21/24.
+//  Copyright © 2024 Oscar Santos. All rights reserved.
+//
+
+import SwiftUI
+import SwiftData
+
+struct FavoritesScreenView: View {
+    @Query var movies: [Movie]
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                ForEach(movies) { movie in
+                    VStack(alignment: .center) {
+                        Spacer()
+                        
+                        Text(movie.title)
+                            .font(.title)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.thinMaterial)
+                    }
+                    .background {
+                        Group {
+                            if let image = UIImage(data: movie.backdropImage) {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .scaledToFit()
+                            } else {
+                                Color.primary
+                            }
+                        }
+                        .ignoresSafeArea()
+                    }
+                }
+            }
+        } detail: {
+            Text("Movie Details")
+        }
+
+    }
+}
+
+#Preview {
+    FavoritesScreenView()
+}
